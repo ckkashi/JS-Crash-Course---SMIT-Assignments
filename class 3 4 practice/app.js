9ce1c1becec0b8dd88793d4aab81be2b59e30161
ckkashi <ckkashi007@gmail.com>
2023-04-08 00:20:03 +0500

    adding practice of class 3 and 4

---
+++ class 3 4 practice/app.js
@@ -0,0 +1,37 @@
+// function welcomeGreet() {
+//     console.log("welcome");
+//     document.write("welcome")
+// }
+// welcomeGreet();
+
+// function welcomeGreet(name) {
+//     console.log("welcome "+name);
+//     document.write("welcome "+name);
+// }
+// welcomeGreet('Kashif');
+
+// function add(val1,val2) {
+//     return val1+val2;
+// }
+// console.log(add(2,4))
+
+// function add(val1,val2) {
+//     return val1+val2;
+// }
+// ans1 = add(4,10);
+// console.log(ans1)
+
+// function add(val1,val2) {
+//     return val1+val2;
+// }
+// ans1 = add(4,10);
+// console.log(add(ans1,5));
+
+// let greet = function(name) {
+//     console.log("welcome "+ name);
+//     return function(lname) {
+//         console.log(name+lname);
+//     }
+// }
+// jack = greet('Kashif')
+// jack(" Shaikh")
